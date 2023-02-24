294a94259e709b615bf6bb50304516e90b325c2f
cristydobson <cristydobson@gmail.com>
2023-02-24 14:24:31 +0100

    Storyboard removed.

--- CryptoTracker/ViewController.swift
+++ CryptoTracker/ViewController.swift
@@ -11,11 +11,8 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
     
     view.backgroundColor = .red
-    
-    print("YAY!!!!")
   }
 
 
