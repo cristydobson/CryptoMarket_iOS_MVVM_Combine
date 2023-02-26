0f50da51ea81954e85d608960e33b5c9de726c44
cristydobson <cristydobson@gmail.com>
2023-02-26 11:58:03 +0100

    Added Chart proces to Data Model

--- CryptoTracker/Models/CryptoDataModel.swift
+++ CryptoTracker/Models/CryptoDataModel.swift
@@ -5,13 +5,22 @@
 //  Created by Cristina Dobson on 2/24/23.
 //
 
-struct CryptoDataModel: Codable {
-  let results: [CryptoMarket]
-}
+
+// MARK: - Crypto Markets
 
 struct CryptoMarket: Codable {
-  let symbol: String
-  let price_24h: Double
-  let volume_24h: Double
-  let last_trade_price: Double
+  let symbol: String?
+  let price_24h: Double?
+  let volume_24h: Double?
+  let last_trade_price: Double?
+  let bids: [CryptoPrice]?
+  let asks: [CryptoPrice]?
 }
+
+
+struct CryptoPrice: Codable {
+  let px: Double?
+  let qty: Double?
+  let num: Int?
+}
+
